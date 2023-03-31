9eac0200525bdfb0bc499d63a03603de8ee0b494
baegyungmin <bjy6439@naver.com>
2023-03-31 16:17:22 +0900

    Add : 게시글 작성 form 생성

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,47 @@
+import axios from "axios";
+import { useState } from "react";
 import "./App.css";
-import styled from "styled-components";
 
 function App() {
-  return <div></div>;
+  const [title, setTitle] = useState<string>("");
+  const [body, setBody] = useState<string>("");
+
+  const onSubmit = () => {
+    axios.post("http://localhost:8080/posts", {
+      title,
+      body,
+    });
+  };
+
+  return (
+    <div className="container">
+      <div className="mb-3">
+        <label className="form-lable ">Title</label>
+        <input
+          className="form-control"
+          value={title}
+          onChange={(e) => {
+            setTitle(e.target.value);
+          }}
+          type="text"
+        />
+      </div>
+      <div className="mb-3">
+        <label className="form-lable ">body</label>
+        <textarea
+          className="form-control"
+          rows={15}
+          value={body}
+          onChange={(e) => {
+            setBody(e.target.value);
+          }}
+        />
+      </div>
+      <button className="btn btn-primary" onClick={onSubmit}>
+        Post
+      </button>
+    </div>
+  );
 }
 
 export default App;
